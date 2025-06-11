357408d86e3bd35b111e383c50c967098d9650ff
NimsaraPasindu <nimsarapasindu63@gmail.com>
2025-06-11 10:56:16 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true,  // listen on all addresses, not just localhost
     port: 3000,  // port number to match docker-compose port mapping
